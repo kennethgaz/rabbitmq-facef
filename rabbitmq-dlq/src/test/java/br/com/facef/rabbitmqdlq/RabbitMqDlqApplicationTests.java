9f57d5a1756a704c583e41e676d0177a945c76f7
kenneth <kg_azevedo@hotmail.com>
2020-09-09 22:42:43 -0300

    add dlq

---
+++ rabbitmq-dlq/src/test/java/br/com/facef/rabbitmqdlq/RabbitMqDlqApplicationTests.java
@@ -0,0 +1,11 @@
+package br.com.facef.rabbitmqdlq;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class RabbitMqDlqApplicationTests {
+  @Test
+  void hello() {
+    Assertions.assertTrue(true);
+  }
+}
